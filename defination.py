89c09868ff1e105a6fc63050224d4893d50aa2a0
charanpreetx <channno786@gmail.com>
2025-08-06 13:25:56 +0530

    Update defination.py

---
+++ defination.py
@@ -0,0 +1,70 @@
+# 23-06-2025.
+# Why learn Python?
+# Python is everywhere now from web applications to ML/AI.
+# small apps to robort.
+
+# What is python?
+# High level programming language.
+# Interpreter based language. Interpreter check code line by line if error ocuur it will stop execution. Faster debugging.
+# Do not need any ; or {}.
+# Relies on identation(space in starting of line)
+
+# Complier: it will execute whole code once.
+
+# print("Hello")
+
+# History.
+# Developed by "Guido van rossum" in 1991.
+# Current version of python is 3.13.5.
+
+# Features.
+# It is popular and easy to use language.
+# Simple syntax similar to english language.
+# Everyone can learn python even those who are new to coding.
+
+
+# Set up environment:-
+# Install python and pip.
+# python is interpreter. 
+# pip have all the packages.
+
+# Visual Studio Code :- IDE help to run python program. Use python as an interpreter. 
+# VSC is free and open source code editor by Microsoft. 
+# Extension.
+
+# Python syntax.
+print("Hello")
+print(2)
+print(5.5)
+
+# How to run?
+# Terminal.
+# Direct
+
+# Python comments.
+# "#" is single line comment.
+
+"""This is Multiline 
+comments using double quotes."""
+
+'''This is multiline 
+comments using single quotes.'''
+
+# Python basics.
+x="Hello"   # String is a collection of characters.
+print(x)
+
+x=25        # integer
+print(x)
+
+x=25.5       # float
+print(x)
+
+z=True      # Boolean (True/False)
+print(z)
+
+# # Variables: are created to store value. Variables are created When we assign any value to them.
+# # Do not need to declare any datatype. We can change values of variables.
+x=4
+x="Hello World"
+print(x)
